113339eb871f31190e4b59b8d9b881070b0efdf2
nunibye <38873297+nunibye@users.noreply.github.com>
2024-04-03 15:40:50 -0700

    Auth/NEXT.js basics

---
+++ peergrader_next_supa/app/layout.tsx
@@ -0,0 +1,28 @@
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "PeerGrader",
+  description: "Grading made easy",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ peergrader_next_supa/app/page.tsx
@@ -0,0 +1,28 @@
+import AuthButton from "../components/AuthButton";
+
+export default async function Index() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <AuthButton />
+        </div>
+      </nav>
+
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        
+        <main className="flex-1 flex flex-col gap-6">
+          <h2 className="font-bold text-4xl mb-4">Peer Grader</h2>
+          
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Footer
+        </p>
+      </footer>
+    </div>
+  );
+}
