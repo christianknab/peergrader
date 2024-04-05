ff8ad46ed6fbcdeaf2283a01b6f7da52a9853c2b
nunibye <38873297+nunibye@users.noreply.github.com>
2024-04-05 00:01:00 -0700

    oauth progress

---
+++ peergrader/app/login/google-signin.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { redirect } from "next/navigation";
+import { useState } from 'react';
+import {supabase} from "../../utils/supabase/client";
+export default function GoogleSignInButton() {
+    const [loading, setLoading] = useState(false);
+    async function OAuthSignIn() {
+        try {
+            setLoading(true);
+            const { error } = await supabase.auth.signInWithOAuth({
+              provider: 'google',
+            });
+      
+            if (error) {
+              console.error('Error signing in with Google:', error);
+              setLoading(false);
+              return;
+            }
+      
+            // Redirect the user to the desired page after successful login
+            await redirect('/protected');
+          } catch (error) {
+            console.error('Error signing in with Google:', error);
+            setLoading(false);
+          }
+    }
+
+    return (
+        <div>
+            <button onClick={OAuthSignIn} disabled={loading}>{loading ? 'Loading...' : 'Sign in with Google'}</button>
+        </div>
+    )
+}
--- peergrader/utils/supabase/client.ts
+++ peergrader/utils/supabase/client.ts
@@ -5,3 +5,5 @@ export const createClient = () =>
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
+
+export const supabase = createClient();
