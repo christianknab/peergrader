2094df7e79a2bc3836d6ab6d0848cfccce163b1c
aasthav12 <120447854+aasthav12@users.noreply.github.com>
2024-04-24 02:14:16 -0700

    ui for student course page, rough

--- peergrader/app/courses/[course_id]/layout.tsx
+++ peergrader/app/courses/[course_id]/layout.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { createClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
 
 interface CourseData {
   name: string;
@@ -13,6 +14,7 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
   const { course_id } = useParams();
   const [courseData, setCourseData] = useState<CourseData | null>(null);
   const supabase = createClient();
@@ -42,11 +44,28 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   }, [course_id]);
 
   return (
-    <div>
-      <header>
-        <h1 className="text-5xl font-bold text-center mb-8 write-blue">{courseData?.name || 'Course Page'}</h1>
-      </header>
-      <main>{children}</main>
+    <div className="w-full min-h-screen flex flex-col">
+      <main>
+        <div className="w-full flex justify-between items-center p-4 light-grey">
+          <button 
+            className="py-2 px-4 rounded-md font-bold no-underline bg-btn-background hover:bg-btn-background-hover"
+            onClick={() => router.push('/dashboard')}>
+              Return to Dashboard
+          </button>
+          <span className="font-bold text-lg">PeerGrader</span>
+        </div>
+        <header>
+          <div className="w-4/5 mx-auto bold-blue rounded-lg">
+            <h2 className="text-5xl font-bold text-left mt-4 mb-8 p-28 text-white">{courseData?.name || 'Course Page'}</h2>
+          </div>
+        </header>
+        <div className="w-4/5 mx-auto">
+          <main>{children}</main>
+        </div>
+      </main>
+      <footer className="w-full font-bold mt-8 light-grey p-4 bg-white text-center">
+        <p>&copy;2024 PeerGrader</p>
+      </footer>
     </div>
   );
 };
--- peergrader/app/courses/[course_id]/studentCoursePage.tsx
+++ peergrader/app/courses/[course_id]/studentCoursePage.tsx
@@ -5,12 +5,16 @@ import { createClient } from '@/utils/supabase/client';
 import Link from 'next/link';
 import ListAsgn from '@/app/courses/[course_id]/ListAsgn';
 import ListStudents from './ListStudents';
+import { useRouter } from 'next/navigation';
+import ListAllAsgn from "@/components/ListAllAsgn";
 
 interface CourseData {
     owner: string;
+    name: string;
 }
 
 export default function StudentCoursePage() {
+    const router = useRouter();
     const supabase = createClient();
     const params = useParams();
     const course_id = params.course_id as string;
@@ -21,7 +25,7 @@ export default function StudentCoursePage() {
             try {
                 const { data, error } = await supabase
                     .from('courses')
-                    .select('owner')
+                    .select('owner, name')
                     .eq('course_id', course_id)
                     .single();
 
@@ -41,23 +45,40 @@ export default function StudentCoursePage() {
     }, [course_id]);
 
     return (
-        <div>
-            {courseData && (
-                <>
-                    <Link href={{ pathname: `${course_id}/create-assignment` }}>
-                        {<button className="py-2 px-4 rounded-md font-bold no-underline bg-btn-background hover:bg-btn-background-hover">
-                            Add assignment</button>}
-                    </Link>
-                </>
+        <main className="flex-1 w-full">
+            <div className="flex gap-8">
+                <div className="flex flex-col w-1/3 rounded-lg overflow-hidden gap-6"> 
+                    <div className="flex flex-col rounded-lg overflow-hidden"> 
+                        <div className="light-blue p-5">
+                            <p className="text-xl text-left font-semibold">Students</p>
+                        </div>
+                        <div className="light-grey flex-grow p-6"> 
+                            <ListStudents course_id={course_id} />
+                        </div>
+                    </div>
 
-            )}
-            <h3 className="mt-4">Asignments: </h3>
-            <ListAsgn course_id={course_id} />
+                    <div className="flex flex-col rounded-lg overflow-hidden"> 
+                        <div className="light-blue p-5">
+                            <p className="text-xl text-left font-semibold">Grades</p>
+                        </div>
+                        <div className="light-grey flex-grow p-6"> 
+                            Grades
+                        </div>
+                    </div>
+                </div>
 
-            <footer className="w-full font-bold mt-8 light-grey p-4 bg-white text-center">
-                <p>&copy;2024 PeerGrader</p>
-            </footer>
-        </div>
+                <div className="flex flex-col w-2/3 gap-6 h-full"> 
+                    <div className="flex flex-col rounded-lg overflow-hidden flex-grow"> 
+                        <div className="light-blue p-5"> 
+                            <p className="text-xl text-left font-semibold">Assignments</p>
+                        </div>
+                        <div className="light-grey flex-grow p-6"> 
+                            <ListAsgn course_id={course_id} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
