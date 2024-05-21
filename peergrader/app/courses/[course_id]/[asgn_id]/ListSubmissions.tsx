c88c2cb197094da4f4215a48000707b5d038d0c6
ericbreh <ericchuang94@gmail.com>
2024-05-20 20:47:54 -0700

    teacher page fixes

--- peergrader/app/courses/[course_id]/[asgn_id]/ListSubmissions.tsx
+++ peergrader/app/courses/[course_id]/[asgn_id]/ListSubmissions.tsx
@@ -4,11 +4,11 @@ import { createClient } from '@/utils/supabase/client';
 import Link from 'next/link';
 
 interface SubmissionData {
-  filename: string;
+  uid: string;
+  email: string;
+  first_name: string;
+  last_name: string;
   file_id: string | null;
-  ownerEmail: string;
-  owner: string;
-  grade: string | null;
 }
 
 interface ListSubmissionsProps {
@@ -25,90 +25,21 @@ export default function ListSubmissions({ course_id, asgn_id }: ListSubmissionsP
   }, [course_id, asgn_id]);
 
   async function fetchSubmissions(course_id: string, asgn_id: string) {
-    const { data: accountCourses, error: accountCoursesError } = await supabase
-      .from('account_courses')
-      .select('uid')
-      .eq('course_id', course_id);
-
-    if (accountCoursesError) {
-      console.error('Error fetching accounts:', accountCoursesError);
-      return [];
+    const { data, error } = await supabase.rpc('get_submissions', { course_id_param: course_id, asgn_id_param: asgn_id });
+    if (error) {
+      console.error('Error fetching submissions:', error);
+      return;
     }
-
-    const uids = accountCourses?.map((ac) => ac.uid) || [];
-
-    const { data: accounts, error: accountsError } = await supabase
-      .from('accounts')
-      .select('email, uid')
-      .in('uid', uids);
-
-    if (accountsError) {
-      console.error('Error fetching accounts:', accountsError);
-      return [];
-    }
-
-    const submissions: SubmissionData[] = await Promise.all(
-      accounts.map(async (account) => {
-        const { data: latestSubmission, error: latestSubmissionError } = await supabase
-          .from('submissions')
-          .select('filename, file_id')
-          .eq('asgn_id', asgn_id)
-          .eq('owner', account.uid)
-          .order('created_at', { ascending: false })
-          .limit(1)
-          .single();
-
-        const { data: average_grade, error: averageGradeError } = await supabase.rpc('calculate_average_grade', { file_id_param: submissions });
-
-        if (latestSubmissionError || averageGradeError) {
-          return {
-            filename: 'No submission',
-            file_id: null,
-            ownerEmail: account.email,
-            owner: account.uid,
-            grade: null,
-          };
-        }
-
-        return {
-          filename: latestSubmission?.filename,
-          file_id: latestSubmission?.file_id,
-          ownerEmail: account.email,
-          owner: account.uid,
-          grade: average_grade,
-        };
-      })
-    );
-
-    return submissions;
+    return data;
   }
 
   return (
     <div>
-      <h3>Submissions:</h3>
       <ul>
         {submissions.map((submission, index) => (
           <li key={index}>
-            {submission.ownerEmail}: {submission.filename}{' '}
-            {submission.file_id && (
-              <>
-                <Link
-                  href={{
-                    pathname: `/courses/${course_id}/${asgn_id}/grade`,
-                    query: {
-                      owner: submission.owner,
-                      file_id: submission.file_id,
-                      filename: submission.filename,
-                    },
-                  }}
-                >
-                  <button style={{ marginLeft: '15px' }} className="px-4 mb-1 rounded-md font-bold no-underline bg-btn-background hover:bg-btn-background-hover">
-                    {submission.grade !== null ? `-- Current Grade: ${submission.grade}` : ''}
-                    {submission.grade !== null ? ' -- Regrade' : 'Grade'}{' '}
-                  </button>
-                </Link>
-              </>
-            )}
+            {submission.first_name} {submission.last_name}: {' '}
+            {submission.file_id && ('Turned in') || ('No Submission')}
           </li>
         ))}
       </ul>
