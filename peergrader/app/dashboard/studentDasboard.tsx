ee50effaa4a7a2ddfe9994b7284ccd8bdc7ccd1e
aasthav12 <120447854+aasthav12@users.noreply.github.com>
2024-05-27 00:05:06 -0700

    ui changes to student dashboard

--- peergrader/app/dashboard/studentDasboard.tsx
+++ peergrader/app/dashboard/studentDasboard.tsx
@@ -55,33 +55,32 @@ export default function StudentDashboardPage() {
   if (isError) { return <div>Error</div>; }
 
   return (
-    <div className="flex flex-col min-h-screen w-full bg-white">
-      <header className="w-full py-3">
-        <h1 className="text-5xl font-bold text-left pl-4 write-blue">Student Dashboard</h1>
-        <hr className="my-1 border-t-2"></hr>
+    <div className="flex flex-col min-h-screen w-full">
+      <header className="w-full py-8">
+        <h1 className="text-6xl font-bold text-left pl-4 write-blue">Student Dashboard</h1>
       </header>
 
       <main className="flex-1 w-full">
-        <div className="px-4 py-0 flex gap-8">
-          <div className="flex flex-col flex-grow rounded-lg overflow-hidden">
-            <div className="light-blue p-5">
-              <p className="text-xl text-left font-semibold write-grey">Assignments</p>
+        <div className="px-4 py-0 flex gap-8 mb-10">
+          <div className="flex flex-col flex-grow rounded-lg overflow-hidden shadow-lg">
+            <div className="white-blue-gradient p-5">
+              <p className="text-xl text-left text-white font-semibold text-white">To-Do Assignments</p>
             </div>
             <div className="light-white flex-grow p-6">
               <StudentListAllAsgn />
             </div>
           </div>
 
-          <div className="flex flex-col flex-grow rounded-lg overflow-hidden">
-            <div className="flex justify-between items-center light-blue p-5">
+          <div className="flex flex-col flex-grow rounded-lg overflow-hidden shadow-lg">
+            <div className="flex justify-between items-center white-blue-gradient p-5">
               <Link
                 href="/courses"
-                className="text-xl text-left font-semibold write-grey"
+                className="text-xl text-left text-white font-semibold text-white"
               >
                 Courses Enrolled
               </Link>
               <button
-                className="bg-blue-500 text-white font-bold py-1 px-4 rounded hover:bg-btn-background-hover"
+                className="light-grey text-black font-semibold py-1 px-4 rounded hover:bg-btn-background-hover"
                 onClick={() => setShowModal(true)}>
                 + Add Course
               </button>
@@ -107,7 +106,7 @@ export default function StudentDashboardPage() {
         </div>
       </main>
 
-      <footer className="w-full font-bold light-grey p-4 bg-white text-center">
+      <footer className="shadow w-full font-bold p-4 bg-white text-center">
         <p>&copy;2024 PeerGrader</p>
       </footer>
 
