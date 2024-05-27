2ee434829bfbf51f78b71a62f9275a9578d11dfb
aasthav12 <120447854+aasthav12@users.noreply.github.com>
2024-05-27 13:18:34 -0700

    removed old nav bar

--- peergrader/app/courses/[course_id]/[asgn_id]/grade/studentGradePage.tsx
+++ peergrader/app/courses/[course_id]/[asgn_id]/grade/studentGradePage.tsx
@@ -370,14 +370,6 @@ export default function StudentGradePage() {
 
         <div style={{ width: `${columnWidth}%` }}>
           <div className='overflow-y-auto h-screen' ref={pdfContainerRef}>
-            <div className="w-full flex justify-between items-center px-4 py-2 light-grey">
-              <button
-                className="py-2 px-4 rounded-md font-bold no-underline bg-btn-background hover:bg-btn-background-hover"
-                onClick={() => router.back()}>
-                Return to Assignment
-              </button>
-              <span className="font-bold text-lg">PeerGrader</span>
-            </div>
             <PDFView fileUrl={publicUrl}
               width={PDFWidth} onPageClick={documentClickHandler}
               annotationMarkers={selectedTab == 1 ? annotationMarkers : []}
