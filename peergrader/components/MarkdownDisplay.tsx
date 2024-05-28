10d33c60f50c0a71f005cd3819f50153ee2d6cc0
nunibye <38873297+nunibye@users.noreply.github.com>
2024-05-28 15:57:04 -0700

    Better tasklist support

--- peergrader/components/MarkdownDisplay.tsx
+++ peergrader/components/MarkdownDisplay.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 
@@ -15,12 +16,21 @@ export default function Markdown({ className, children }: MarkdownProps) {
         h5: ({ node, ...props }) => { return (<h5 {...props} style={{ fontWeight: 500, lineHeight: 1.111, fontSize: 17 }} />) },
         h6: ({ node, ...props }) => { return (<h6 {...props} style={{ fontWeight: 500, lineHeight: 1.125, fontSize: 16 }} />) },
         p: ({ node, ...props }) => { return (<p {...props} style={{ lineHeight: 1.5, fontSize: 16 }} />) },
-        a: ({ node, ...props }) => { return (<a {...props} style={{ lineHeight: 1.5, fontSize: 16, color: "blue" }} target="_blank" />) },
-        ul: ({ node, ...props }) => <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }} {...props} />,
-        ol: ({ node, ...props }) => <ol style={{ listStyleType: 'decimal', paddingLeft: '20px' }} {...props} />,
-        li: ({ node, ...props }) => <li style={{ marginBottom: '3px' }} {...props} />,
+        a: ({ node, ...props }) => { return (<a {...props} style={{ lineHeight: 1.5, fontSize: 16, color: "blue" }} target={props.href?.startsWith("#") ? "" : "_blank"} />) },
+        ul: ({ node, ...props }) => {
+
+            return (<ul style={{ listStyleType: 'disc', paddingLeft: '20px' }} {...props} />);
+        },
+        ol: ({ node, ...props }) => {
+
+            return (<ol style={{ listStyleType: 'decimal', paddingLeft: '20px' }} {...props} />);
+        },
+        li: ({ node, ...props }) => {
+            const hasCheckbox = props.className === "task-list-item";
+            return (<li style={{ marginBottom: '3px', listStyleType: hasCheckbox ? "none" : "inherit", marginLeft: hasCheckbox ? -17: 0}} {...props} />);
+        },
         table: ({ node, ...props }) => <table {...props} className="table-auto" />,
-        th: ({ node, ...props }) => <th {...props} className="px-4 py-2 border"/>,
+        th: ({ node, ...props }) => <th {...props} className="px-4 py-2 border" />,
         td: ({ node, ...props }) => <td {...props} className="border px-4 py-2" />,
     }}>
         {children}
--- peergrader/components/icons/CollapseIndicator.tsx
+++ peergrader/components/icons/CollapseIndicator.tsx
@@ -1,12 +1,12 @@
 const CollapseIndicator = ({isOpen, size = 10}:{isOpen: boolean, size:number}) => (isOpen ?
     (<svg name="IconArrowOpenDown" viewBox="0 0 1920 1920" rotate="0" width={size} height={size} aria-hidden="true" role="presentation" focusable="false">
 
-        <path d="m.08 568.063 176.13-176.13 783.988 783.864 783.74-783.864 176.129 176.13-959.87 960.118z" fill-rule="evenodd">
+        <path d="m.08 568.063 176.13-176.13 783.988 783.864 783.74-783.864 176.129 176.13-959.87 960.118z" fillRule="evenodd">
 
         </path>
 
     </svg>) : (<svg name="IconArrowOpenEnd" viewBox="0 0 1920 1920" rotate="0" width={size} height={size} aria-hidden="true" role="presentation" focusable="false">
-        <path d="M568.13.012 392 176.142l783.864 783.989L392 1743.87 568.13 1920l960.118-959.87z" fill-rule="evenodd">
+        <path d="M568.13.012 392 176.142l783.864 783.989L392 1743.87 568.13 1920l960.118-959.87z" fillRule="evenodd">
 
         </path>
     </svg>)
