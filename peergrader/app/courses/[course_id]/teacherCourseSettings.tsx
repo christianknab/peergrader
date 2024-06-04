6f6421b0b91284d965de517bb75cf309af4cd9b8
christianknab <christiantknab@gmail.com>
2024-06-03 18:31:56 -0700

    that was a nasty merge

--- peergrader/app/courses/[course_id]/teacherCourseSettings.tsx
+++ peergrader/app/courses/[course_id]/teacherCourseSettings.tsx
@@ -78,101 +78,6 @@ export default function TeacherCourseSettings({ courseId, courseData }: {
     };
 
     return (
-        <div className="flex flex-col overflow-hidden w-full">
-            <form onSubmit={handleSubmit} className="">
-                <label className="block text-gray-700 font-bold mb-2">Basic Settings</label>
-                <div className="px-4">
-                    <div className="flex space-x-4">
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="Course Name"
-                            value={courseData?.name}
-                            name="name"
-                            type="text"
-                            onChange={() => setFormEdited(true)}
-                            isRequired={true}
-                        />
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="Course Number"
-                            value={courseData?.number}
-                            name="number"
-                            type="text"
-                            onChange={() => setFormEdited(true)}
-                            isRequired={false}
-                        />
-                    </div>
-                    <div className="flex space-x-4">
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="Start Date"
-                            value={startDate}
-                            name="start_date"
-                            type="date"
-                            onChange={handleStartDateChange}
-                            isRequired={true}
-                        />
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="End Date"
-                            value={endDate}
-                            name="end_date"
-                            type="date"
-                            onChange={handleEndDateChange}
-                            isRequired={true}
-                        />
-                    </div>
-                </div>
-                <div className='px-4 pb-2'>
-                    <button
-                        type="submit"
-                        className={`${formEdited ? 'bg-blue-500' : 'bg-gray-400'} ${formEdited ? 'hover:bg-blue-700' : ''} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
-                        disabled={!formEdited}
-                    >
-                        Save
-                    </button>
-                </div>
-            </form>
-            <label className="block text-gray-700 font-bold mb-2">Student Settings</label>
-            <div className="px-4">
-                <div className="flex">
-                    {/* TODO: need better way to align things */}
-                    <div className="flex space-x-2 flex-shrink-0">
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="Join Link"
-                            value={`peergrader.vercel.app/courses?code=${joinCode}`}
-                            name="name"
-                            type="text"
-                            isRequired={true}
-                            disabled={true}
-                        />
-                        <button className='pt-3' onClick={() => copyToClipboard(joinLink)}>
-                            <CopyIcon />
-                        </button>
-                    </div>
-                    <div className="flex px-4 space-x-2">
-                        <InputFieldForm
-                            format="mb-4 flex-1"
-                            label="Join Code"
-                            value={joinCode}
-                            name="name"
-                            type="text"
-                            isRequired={true}
-                            disabled={true}
-                        />
-                        <button className='pt-3' onClick={() => copyToClipboard(joinCode)}>
-                            <CopyIcon />
-                        </button>
-                        <div className='pt-9'>{copySuccess && (
-                            <span className="text-blue-500">{copySuccess}</span>
-                        )}</div>
-                    </div>
-                </div>
-            </div>
-            <label className="block text-gray-700 font-bold mb-2">Grading</label>
-            <div className='px-2 w-1/4'><ExportGrades /></div>
-            <div className="text-red-500">{dateError}</div>
         <div className="flex flex-col w-full gap-6 h-full">
             <div className="flex flex-col rounded-lg overflow-hidden flex-grow">
                 <div className="white-blue-gradient p-5">
@@ -224,24 +129,56 @@ export default function TeacherCourseSettings({ courseId, courseData }: {
                                 />
                             </div>
                         </div>
-                        <label className="block text-gray-700 font-bold mb-2">Student Settings</label>
-
-                        <JoinShareButton joinCode={courseData?.join_code} />
-                        <div className="text-red-500">{dateError}</div>
-                        <div>
-                            {formEdited ? (
-                                <button
-                                    type="submit"
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                >
-                                    Save
-                                </button>
-                            ) : (
-                                <div className="py-5"></div>
-                            )}
+                        <div className='px-4 pb-2'>
+                            <button
+                                type="submit"
+                                className={`${formEdited ? 'bg-blue-500' : 'bg-gray-400'} ${formEdited ? 'hover:bg-blue-700' : ''} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+                                disabled={!formEdited}
+                            >
+                                Save
+                            </button>
                         </div>
                     </form>
-                    <ExportGrades />
+                    <label className="block text-gray-700 font-bold mb-2">Student Settings</label>
+                    <div className="px-4">
+                        <div className="flex">
+                            {/* TODO: need better way to align things */}
+                            <div className="flex space-x-2 flex-shrink-0">
+                                <InputFieldForm
+                                    format="mb-4 flex-1"
+                                    label="Join Link"
+                                    value={`peergrader.vercel.app/courses?code=${joinCode}`}
+                                    name="name"
+                                    type="text"
+                                    isRequired={true}
+                                    disabled={true}
+                                />
+                                <button className='pt-3' onClick={() => copyToClipboard(joinLink)}>
+                                    <CopyIcon />
+                                </button>
+                            </div>
+                            <div className="flex px-4 space-x-2">
+                                <InputFieldForm
+                                    format="mb-4 flex-1"
+                                    label="Join Code"
+                                    value={joinCode}
+                                    name="name"
+                                    type="text"
+                                    isRequired={true}
+                                    disabled={true}
+                                />
+                                <button className='pt-3' onClick={() => copyToClipboard(joinCode)}>
+                                    <CopyIcon />
+                                </button>
+                                <div className='pt-9'>{copySuccess && (
+                                    <span className="text-blue-500">{copySuccess}</span>
+                                )}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <label className="block text-gray-700 font-bold mb-2">Grading</label>
+                    <div className='px-2 w-1/4'><ExportGrades /></div>
+                    <div className="text-red-500">{dateError}</div>
 
                 </div>
             </div>
